8d624b952943d1426cb55f03eb85c709c41ceabd
HXD <1806191732@qq.com>
2023-02-22 20:36:14 +0800

    docs(): 修复使用方法的错误

--- README.md
+++ README.md
@@ -1,37 +1,40 @@
-<h1 align="center">vite-plugin-build-time</h1>
+<h1 align="center">vite-plugin-build-version</h1>
 
-vite项目记录下本次打包的时间，供生产环境使用
-
-如果在开发环境使用，获取到的是本次启动的时间
+vite项目记录下本次打包的package.json版本
 
 ## 安装
 
 通过npm下载
 
-```base
-npm install --save-dev vite-plugin-build-time
+```shell
+npm install vite-plugin-build-version --save-dev
 ```
 
 ## 用法
 
 ```js
 // vite.config.js
-import buildTimePlugin from 'vite-plugin-build-time'
+import { defineConfig } from 'vite';
 
-// ...
-plugins: [buildTimePlugin()]
-// 或者 在开发模式使用
-plugins: [buildTimePlugin(['development'])]
-// 或者 在自定义模式使用
-plugins: [buildTimePlugin(['test'])]
+import buildVersionPlugin from 'vite-plugin-build-version';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // ...其他配置
+  plugins: [
+    // 获取打包package中的打包版本
+    buildVersionPlugin(JSON.stringify(process.env.npm_package_version), true),
+  ],
+});
+
 // ...
 ```
 
 ```vue
 <!-- src/App.vue -->
 <script>
-import { getBuildTime } from 'vite-plugin-build-time'
-// 获取打包时间
-console.log(getBuildTime())
+import { getBuildVersion } from 'vite-plugin-build-time';
+
+console.log('BuildVersion==>', getBuildVersion());
 </script>
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,51 @@
 import { Plugin } from 'vite'
 
 const buildVersionKey = '__VITE_PLUGIN_BUILD_VERSION__'
+const virtualGlobal = {
+  [buildVersionKey]: '0.0.0'
+}
+
+interface GetGlobalThis {
+  (): typeof globalThis & typeof virtualGlobal
+}
+
+// @ts-ignore
+const getGlobalThis: GetGlobalThis = () => {
+  if (typeof globalThis !== 'undefined') return globalThis
+  if (typeof self !== 'undefined') return self
+  if (typeof window !== 'undefined') return window
+  if (typeof global !== 'undefined') return global
+  return virtualGlobal
+}
 
 /**
- * @param modes 除了production之外，还要在什么模式下运行此插件。比如`buildVersionPlugin(['development'])`
- * @param version 从package中获取的版本号
+ * @param version package.json版本号
+ * @param serve 是否作用于serve阶段
  */
-const buildVersionPlugin = (modes: string[] = [], version: string): Plugin => {
-  let _mode = ''
+const buildVersionPlugin = (version: string, serve?: boolean): Plugin => {
+  let isBuild = false
   return {
-    name: 'vite-plugin-build-version',
-    config(uc, { mode }) {
-      _mode = mode
+    name: 'vite-plugin-build-time',
+    config(uc, { command: viteCommand }) {
+      isBuild = viteCommand === 'build'
     },
-    transformIndexHtml() {
-      if (_mode !== 'production' && !modes.includes(_mode)) return
-      return [{
-        tag: 'script',
-        children: `window.${ buildVersionKey }=${ version }`
-      }]
+    transformIndexHtml(html) {
+      if (!isBuild && !serve) return html
+      let newHtml = html
+      const pattern = /<head>\r?\n+\s*</
+      const execArr = pattern.exec(newHtml)
+      let spaceStr = ''
+      if (execArr) {
+        let [val] = execArr
+        val = val.substring(6, val.length - 1)
+        spaceStr = val.replace(/\r/g, '').replace(/\n/g, '')
+      }
+      let joinStr = spaceStr ? `\n${ spaceStr }` : '\n  '
+      newHtml = newHtml.replace(`<head>`, `<head>${ joinStr }<script>window.${ buildVersionKey }=${ version }</script>`)
+      return newHtml
     }
   }
 }
 
-export const getBuildVersion = () => {
-  if (typeof window === 'undefined') {
-    console.warn('[vite-plugin-build-version] -> window未定义')
-    return '0.0.0'
-  }
-  const __window__ = window as typeof window & { [buildVersionKey]?: string }
-  if (!__window__[buildVersionKey]) {
-    console.warn('[vite-plugin-build-version] -> 无法获取打包时间，请查看文档')
-    return '0.0.0'
-  }
-  return __window__[buildVersionKey] as string
-}
-
+export const getBuildVersion = () => getGlobalThis()[buildVersionKey]
 export default buildVersionPlugin
